acb2661e457612360402289a8c1840757a52e612
DanyOuaras <41428518+DanyOuaras@users.noreply.github.com>
2018-08-27 09:51:30 +0200

    Update Cmd Windows.cs

--- CSharp/CSharp/Cmd Windows.cs
+++ CSharp/CSharp/Cmd Windows.cs
@@ -25,30 +25,6 @@ namespace WindowsAPP
             process.StandardInput.WriteLine("PowerShell.exe -ExecutionPolicy Bypass \\\\gpso-srv-sipc\\Sources\\Win10\\DEP\\01-ApplicationW10\\UninstalCurrentUserApp.ps1");
             Thread.Sleep(60000);
 
-            Console.WriteLine("Désintallation des Apps Windows pour tous les utilisateurs...\n");
-            process.StandardInput.WriteLine("PowerShell.exe -ExecutionPolicy Bypass \\\\gpso-srv-sipc\\Sources\\Win10\\DEP\\01-ApplicationW10\\UninstalOnlineApp.ps1");
-            Thread.Sleep(60000);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Désinstallation des Apps Windows Terminé.\n");
-
-            process.StandardInput.WriteLine("PowerShell.exe -ExecutionPolicy Bypass \\\\gpso-srv-sipc\\Sources\\Win10\\DEP\\01-ApplicationW10\\InstalAppBase.ps1");
-
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Installation de la Calculatrice, Visioneuse de photos, StickyNotes... \n");
-            Thread.Sleep(5000);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Installation de la Calculatrice Terminé.");
-
-            Thread.Sleep(5000);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Installation de la Visioneuse de photos Terminé.");
-
-            Thread.Sleep(10000);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Installation de StickyNotes Terminé.\n");
-
-            Console.WriteLine("Installation Terminé.");
             process.StandardInput.Flush();
             process.StandardInput.Close();
             process.WaitForExit();
